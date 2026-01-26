ee2a7fff181459bff04d2177d6e4651986e7051f
smirnovav <smrinovavcsoft@gmail.com>
2026-01-26 23:47:26 +0300

    developing ModalDialogComponent

---
+++ FarvaterWeb/Components/ModalDialogComponent.cs
@@ -0,0 +1,23 @@
+﻿using AventStack.ExtentReports;
+using FarvaterWeb.Base;
+using Microsoft.Playwright;
+using Serilog;
+using FarvaterWeb.Extensions;
+
+namespace FarvaterWeb.Components
+{
+    public class ModalDialogComponent : BaseComponent
+    {
+        private ILocator ModalDialogRoot => Page.Locator("[data-signature='modal-dialog-content']");
+         
+        public ModalDialogComponent(IPage page, ILogger logger, ExtentTest test)
+            : base(page, logger, test) { }
+
+        public SmartLocator ButtonWithText(string text)
+        {
+            var locator = ModalDialogRoot.Locator("");
+
+            return new SmartLocator(locator, text);
+        }
+    }
+}

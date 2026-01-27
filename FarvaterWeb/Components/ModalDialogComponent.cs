ba759ed53511d8048f6b35244c8e638d90ecf9b7
smirnovav <smrinovavcsoft@gmail.com>
2026-01-27 23:55:18 +0300

    developed ModalDialogComponent

--- FarvaterWeb/Components/ModalDialogComponent.cs
+++ FarvaterWeb/Components/ModalDialogComponent.cs
@@ -9,15 +9,40 @@ namespace FarvaterWeb.Components
     public class ModalDialogComponent : BaseComponent
     {
         private ILocator ModalDialogRoot => Page.Locator("[data-signature='modal-dialog-content']");
-         
+
         public ModalDialogComponent(IPage page, ILogger logger, ExtentTest test)
             : base(page, logger, test) { }
 
-        public SmartLocator ButtonWithText(string text)
+        // Находим кнопку внутри модалки по тексту
+        public SmartLocator Button(string text)
         {
-            var locator = ModalDialogRoot.Locator("");
+            var locator = ModalDialogRoot
+                .Locator("[data-signature='button-wrapper']")
+                .Filter(new() { HasText = text });
 
-            return new SmartLocator(locator, text);
+            return new SmartLocator(locator, text, "Кнопка", _componentName, Page);
+        }
+
+        // Находим инпут по его лейблу (Наименование, Код и т.д.)
+        public ILocator GetInput(string labelText)
+        {
+            return ModalDialogRoot
+                .Locator("[data-signature='input-field-wrapper']")
+                .Filter(new() { Has = Page.Locator("[data-signature='input-text-title']", new() { HasText = labelText }) })
+                .Locator("[data-signature='input-field-input']");
+        }
+
+        public async Task ClickButtonAsync(string text)
+        {
+            await Do($"Нажатие кнопки '{text}'", async () =>
+            {
+                await Button(text).Locator.ClickAsync();
+                // Если кнопка закрывает окно (Добавить/Отмена), ждем исчезновения
+                if (text == "Добавить" || text == "Отмена")
+                {
+                    await ModalDialogRoot.WaitForAsync(new() { State = WaitForSelectorState.Hidden });
+                }
+            });
         }
     }
 }

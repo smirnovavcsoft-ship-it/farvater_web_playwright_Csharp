4d8d3b5c5054738b7a92ad744cb55be94cbaeeae
smirnovav <smrinovavcsoft@gmail.com>
2026-02-10 22:24:14 +0300

    one more commit

--- FarvaterWeb/Data/UserModel.cs
+++ FarvaterWeb/Data/UserModel.cs
@@ -8,5 +8,29 @@ namespace FarvaterWeb.Data
 {
     public class UserModel
     {
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public string? middleName { get; set; } = "";
+        public string? description { get; set; } // Обычно lastName + firstName
+        public string? login { get; set; }
+        public string? mail { get; set; } = "";
+        public string? phone { get; set; } = "";
+        public string? personnelNumber { get; set; } = "";
+        public bool isDisabled { get; set; } = false;
+        public bool isLeader { get; set; } = false;
+        public bool isDomainUser { get; set; } = false;
+        public bool isSysadmin { get; set; } = false;
+        public bool isExternal { get; set; } = false;
+        public string? language { get; set; } = "ru";
+
+        // Правила (права доступа)
+        public Dictionary<string, UserRuleDto> rules { get; set; } = new();
+    }
+
+    public class UserRuleDto
+    {
+        public bool value { get; set; } = false;
+        public bool byGroup { get; set; } = false;
+        public List<string> groups { get; set; } = new();
     }
 }
